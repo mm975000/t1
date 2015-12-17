266f4c22a1775adf2d5506d647c56b087cd10d49
mm975000 <mepower78@yahoo.co.uk>
2015-12-17 21:41:14 +0300

    gg
    ggg

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express=require("express");
 var favicon = require('serve-favicon');
 var app=express();
-app.use(favicon(__dirname + '/public/flower_01.png'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.get("/",function(req,res){
   res.send("Hello World!");
 });
