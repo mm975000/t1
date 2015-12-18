041d9ad488027f39aed64e3c6e90273b661ebbb2
mm975000 <mepower78@yahoo.co.uk>
2015-12-18 09:31:30 +0300

    xcccc
    cccc

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.set("veiws",__dirname+"/views");
 app.get("/",function(req,res){
   res.send("Hello World!");
 });
+var port=process.env.PORT||80;
+var ip=process.env.IP||"0.0.0.0";
+server.listen(port,ip,function(){
+	console.log("server is on!");
+});
 
 //
 //
