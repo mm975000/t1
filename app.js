dcdc503676b0edcba47da2dda13408f2ae9de9f7
mm975000 <mepower78@yahoo.co.uk>
2015-12-18 08:00:17 +0300

    aaa
    aaaa

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express=require("express");
 var favicon = require('serve-favicon');
+var http=require("http");
+
 var app=express();
+var server=http.Server(app);
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.get("/",function(req,res){
   res.send("Hello World!");
+});
+server.listen(8080,function(){
+	console.log("server is open at 8080");
 });
 //
 //
