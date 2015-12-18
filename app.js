89462c04416f02b191afebc2680661833c537db2
mm975000 <mepower78@yahoo.co.uk>
2015-12-18 10:52:05 +0300

    aaa
    aaa

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var http=require("http");
 var cons=require("consolidate");
 var swig=require("swig");
 var app=express();
-var server=http.Server(app);
+//var server=http.Server(app);
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.engine("html",cons.swig);
 app.set("view engine","html")
