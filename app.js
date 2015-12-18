5f4df0be0fa06172046eca525d15c01cf1398fad
mm975000 <mepower78@yahoo.co.uk>
2015-12-18 09:38:19 +0300

    aa
    aaa

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ app.set("view engine","html")
 app.set("veiws",__dirname+"/views");
 
 app.get("/",function(req,res){
-  res.send("Hello World!");
+  res.render("index",{"title":"Hello World!"});
+  res.end();
 });
 var port=process.env.PORT||80;
-var ip=process.env.IP||"0.0.0.0";
+var ip=process.env.IP||"127.0.0.1";
 server.listen(port,ip,function(){
 	console.log("server is on!");
 });
